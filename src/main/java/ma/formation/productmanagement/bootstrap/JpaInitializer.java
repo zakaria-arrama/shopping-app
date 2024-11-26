ef87b419e5e396a7a0b75fe073624c37a6a94560
zakaria-arrama <zakaria.arrama@gmail.com>
2024-11-26 12:22:04 +0100

    Add Customer entity

---
+++ src/main/java/ma/formation/productmanagement/bootstrap/JpaInitializer.java
@@ -0,0 +1,27 @@
+package ma.formation.productmanagement.bootstrap;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class JpaInitializer implements ServletContextListener{
+
+    private EntityManagerFactory emf;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        emf = Persistence.createEntityManagerFactory("shoppingAppPersistenceUnit");
+        System.out.println("JPA Initialized with Persistence Unit: your-persistence-unit");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+
+}
---
+++ src/main/java/ma/formation/productmanagement/domain/Customer.java
@@ -0,0 +1,11 @@
+package ma.formation.productmanagement.domain;
+
+public class Customer {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+}
